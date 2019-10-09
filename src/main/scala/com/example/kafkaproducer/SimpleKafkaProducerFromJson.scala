f739ecc6093a248f4c62b4ee1d867c9bd85920f2
akhilanandbv003 <akhilanand.bv@gmail.com>
2019-10-08 22:59:50 -0500

    added json producer

---
+++ src/main/scala/com/example/kafkaproducer/SimpleKafkaProducerFromJson.scala
@@ -0,0 +1,30 @@
+package com.example.kafkaproducer
+
+import com.example.kafkaproducer.utils.FileUtils
+import com.typesafe.scalalogging.LazyLogging
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+
+object SimpleKafkaProducerFromJson extends App
+  with FileUtils
+  with LazyLogging {
+
+  val numberOfRecords = 1
+  val topicName = "university.student.dev.v1.json"
+
+  val data = readFromFile("src/main/resources/json/student.json")
+    recordProducer(1, data, topicName)
+
+
+  def recordProducer(numberOfRecords: Int, data: String, topic: String) = {
+    val producer = new KafkaProducer[Nothing, String](ProducerProperties.buildProducerProperties)
+
+    for (i <- 1 to numberOfRecords) {
+      val record = new ProducerRecord(topic, data)
+      val recordMetadata = producer.send(record).get()
+      logger.info(s"Produced records at offset ${recordMetadata.offset} of partition: ${recordMetadata.partition} at ${recordMetadata.timestamp()}")
+
+    }
+  }
+
+
+}
